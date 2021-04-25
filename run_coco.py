0348106dad742004afa168aafb2eac56952324e4
ErinRuslan <erinsoraya26@gmail.com>
2021-04-25 23:34:47 +0800

    add more data

--- run_coco.py
+++ run_coco.py
@@ -1,3 +1,9 @@
+import os
+os.system('pip install cocopp')
+
 import cocopp
+import webbrowser
 
 cocopp.main('DEPSOdatamax50000 SimpleDEdatamax50000')
+
+webbrowser.open("file://" + os.getcwd() + "/ppdata/index.html")
